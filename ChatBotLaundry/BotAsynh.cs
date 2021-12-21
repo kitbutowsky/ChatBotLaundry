c48b132da2411b7fbf3b004d7b5cab7a311740e4
Daniil.Kovalenko@urfu.me <Daniil.Kovalenko@urfu.me>
2021-12-21 17:50:25 +0500

    Улучшена структура программы

--- ChatBotLaundry/BotAsynh.cs
+++ ChatBotLaundry/BotAsynh.cs
@@ -82,18 +82,18 @@ namespace ChatBotLaundry
             {
                 case "adm":
                     session.SendMessage(user.ID, "Выберите действие:");
-                    session.SendInlineButtons(user.ID, GetAdminMenuButtons());
+                    session.SendInlineButtons(user.ID, GetButtons.Adm());
                     return;
                 case "ad":
                     session.SendMessage(user.ID, "Выберите действие:");
-                    session.SendInlineButtons(user.ID, GetAdminButtons());
+                    session.SendInlineButtons(user.ID, GetButtons.Ad());
                     return;
                 case "us":
-                    session.SendInlineButtons(user.ID, GetAdminUsersButtons());
+                    session.SendInlineButtons(user.ID, GetButtons.Us());
                     return;
                 case "usrs":
                     session.SendMessage(user.ID, ListToNumerableStringList(user.adminIdsList.Item1));
-                    session.SendInlineButtons(user.ID, GetAdminStatusButtons(user.adminIdsList.Item2));
+                    session.SendInlineButtons(user.ID, GetButtons.Usrs(user.adminIdsList.Item2));
                     return;
                 case "add":
                     session.SendMessage(user.ID, ListToNumerableStringList(user.adminIdsList.Item1));
@@ -107,7 +107,7 @@ namespace ChatBotLaundry
                     return;
                 case "l":
                     session.SendMessage(user.ID, "Выберите действие:");
-                    session.SendInlineButtons(user.ID, GetAdminLaundryButtons());
+                    session.SendInlineButtons(user.ID, GetButtons.L());
                     return;
                 case "pas":
                     session.SendButtons(user.ID, new List<List<(string, string)>>());
@@ -122,11 +122,11 @@ namespace ChatBotLaundry
                     session.SendButtons(user.ID, new List<List<(string, string)>>());
                     return;
                 case "infadc":
-                    session.SendInlineButtons(user.ID, GetСonfirmationButtons());
+                    session.SendInlineButtons(user.ID, GetButtons.Infadc());
                     return;
                 case "time":
                     session.SendMessage(user.ID, Data.WashesHoursToString());
-                    session.SendButtons(user.ID, GetTimeAdButtons());
+                    session.SendButtons(user.ID, GetButtons.Time());
                     return;
                 case "n":
                     session.SendMessage(user.ID, Data.AllNotesToStringList());
@@ -189,21 +189,21 @@ namespace ChatBotLaundry
                     return;
                 case "cl":
                     session.SendMessage(user.ID, "Выберите действие:");
-                    session.SendButtons(user.ID, GetClientMenuButtons(user));
+                    session.SendButtons(user.ID, GetButtons.Cl(user));
                     return;
                 case "cln":
                     session.SendMessage(user.ID, "Запись");
-                    session.SendButtons(user.ID, GetDayButtons(user.Status));
+                    session.SendButtons(user.ID, GetButtons.Cln(user.Status));
+                    return;
+                case "clnd":
+                    session.SendButtons(user.ID, GetButtons.Clnd(user.note[0]));
+                    return;
+                case "clndt":
+                    session.SendButtons(user.ID, GetButtons.Clndt(user.note[0], user.note[1]));
                     return;
                 case "cldn":
                     session.SendMessage(user.ID, "Отмена записи");
-                    session.SendButtons(user.ID, GetNotesButtons(user));
-                    return;
-                case "clnd":
-                    session.SendButtons(user.ID, GetTimeButtons(user.note[0]));
-                    return;
-                case "clndt":
-                    session.SendButtons(user.ID, GetAmountButtons(user.note[0], user.note[1]));
+                    session.SendButtons(user.ID, GetButtons.Cldn(user));
                     return;
             }
         }
@@ -566,216 +566,6 @@ namespace ChatBotLaundry
                 i++;
             }
             return stringListIds;
-        }
-
-
-
-        //методы получения кнопок
-        private static List<List<(string, string)>> GetСonfirmationButtons()
-        {
-            var buttons = new List<List<(string, string)>>{
-                new List<(string, string)>{
-                    ("Отмена", "b")
-                }
-            };
-            if (Data.NewInfo.Length != 0)
-                buttons.Insert(0, new List<(string, string)>
-                { ("Сохранить изменения" , "save")
-                });
-            return buttons;
-        }
-
-        private static List<List<(string, string)>> GetAdminLaundryButtons()
-        {
-            var buttons = new List<List<(string, string)>>{
-                new List<(string, string)>
-                {
-                    ("Изменить информацию о прачке", "infad")
-                },
-                new List<(string, string)>{
-                    ("Изменить время", "time")
-                },
-                new List<(string, string)>
-                {
-                    ("Изменить количество машинок", "w")
-                },
-                new List<(string, string)>
-                {
-                    ("Изменить пароль", "pas")
-                }
-            }; 
-            return buttons;
-        }
-
-        private static List<List<(string, string)>> GetAdminButtons()
-        {
-            var userAdminButtons = new List<List<(string, string)>>{
-                new List<(string, string)>
-                {
-                    ("Пользователи", "us")
-                },
-                new List<(string, string)>{
-                    ("Прачка", "l")
-                }
-            };
-            if (Data.Notes.Count != 0)
-                userAdminButtons.Add(new List<(string, string)>
-                {
-                    ("Записи", "n" )
-                });
-            return userAdminButtons;
-        }
-
-        private static List<List<(string, string)>> GetTimeAdButtons()
-        {
-            var buttons = new List<List<(string, string)>> {};
-            var i = -1;
-            for (var t = 8; t < 23; t++)
-            {
-                if ((t - 8) % 5 == 0)
-                {
-                    buttons.Add(new List<(string, string)>());
-                    i++;
-                }
-                buttons[i].Add((t.ToString() + ":00", t.ToString()));
-            }
-            return buttons;
-        }
-
-        private static List<List<(string, string)>> GetNotesButtons(User user)
-        {
-            var notes = Data.Notes.FindAll(delegate (TimeNote note)
-            {
-                return note.UserID == user.ID;
-            }
-            );
-            var buttons = new List<List<(string, string)>>();
-            for (var i = 0; i < notes.Count; i++)
-            {
-                var button = new List<(string, string)> { (notes[i].ToString() + "\n", i.ToString()) };
-                buttons.Add(button);
-            }
-            return buttons;
-        }
-
-        private static List<List<(string, string)>> GetAmountButtons(int day, int time)
-        {
-            var buttons = new List<List<(string, string)>>();
-            for (var i = 1; i < Data.Days[day].EmptyTimes[time] + 1; i++)
-            {
-                var button = new List<(string, string)> { (i.ToString() + ". ", i.ToString()) };
-                buttons.Add(button);
-            }
-            return buttons;
-        }
-
-        private static List<List<(string, string)>> GetDayButtons(int status)
-        {
-            var buttons = new List<List<(string, string)>>();
-            for (var d = 0; d < Data.Days.Count; d++)
-                if (Data.Days[d].IsFree && ((Data.Days[d].AvailableForSSK && status != 0) || (!Data.Days[d].AvailableForSSK && status == 0)))
-                {
-                    var button = new List<(string, string)> { (Data.Days[d].DayOfWeekR + " " + Data.Days[d].EmptySpaces.ToString(), d.ToString()) };
-                    buttons.Add(button);
-                }
-            return buttons;
-        }
-
-        private static List<List<(string, string)>> GetTimeButtons(int day)
-        {
-            var buttons = new List<List<(string, string)>>();
-            for (var i = 0; i < Data.Days[day].HoursWashesTable.GetLength(0); i++)
-            {
-                if (Data.Days[day].EmptyTimes[i] != 0)
-                {
-                    var button = new List<(string, string)> { (Data.WashesHours[i].ToString() + ":00 кол-во свободных машинок:" + Data.Days[day].EmptyTimes[i].ToString(), i.ToString() )};
-                    buttons.Add(button);
-                }
-            }
-            return buttons;
-        }
-
-        private static List<List<(string, string)>> GetClientMenuButtons(User user)
-        {
-            var clientMenuButtons = new List<List<(string, string)>> {
-                new List<(string, string)>{
-                    ("Выкл уведомления", "clnt"),
-                    ("FAQ", "info")
-                }
-            };
-
-            if (Data.Notes.FindIndex(delegate (TimeNote note)
-            {
-                return note.UserID == user.ID;
-            }
-            ) != -1)
-                clientMenuButtons.Insert(0, new List<(string, string)> { ("Отмена", "cldn") });
-            if (Data.Days.FindIndex(delegate (Day day)
-            {
-                return (day.EmptySpaces != 0) && (day.AvailableForSSK == (user.Status == 1));
-            }
-            ) != -1)
-                clientMenuButtons.Insert(0, new List<(string, string)>{("Записаться в прачечную", "cln")} );
-            if (!user.NotificationStatus)
-            {
-                var repl = clientMenuButtons[^1][0];
-                repl.Item1 = "Вкл уведомления";
-                clientMenuButtons[^1][0] = repl;
-            }
-            else
-            {
-                var repl = clientMenuButtons[^1][0];
-                repl.Item1 = "Выкл уведомления";
-                clientMenuButtons[^1][0] = repl;
-            }
-            if (user.Status != 3 && user.Status != 2)
-            {
-                clientMenuButtons.Add(new List<(string, string)>{("Выйти", "b")});
-            }
-            return clientMenuButtons;
-        }
-
-        private static List<List<(string, string)>> GetAdminMenuButtons()
-        {
-            var buttons = new List<List<(string, string)>> {
-                new List<(string, string)>{
-                    ("Функции клиента", "cl"),
-                    ("Функции открывающего", "op")
-                },
-                new List<(string, string)>{
-                    ("Администрирование", "ad"),
-                    ("Отчетность", "re")
-                },
-                new List<(string, string)>{
-                    ("Выйти", "b")
-                }
-            };
-            return buttons;
-        }
-
-        private static List<List<(string, string)>> GetAdminUsersButtons()
-        {
-            var buttons = new List<List<(string, string)>> { 
-                new List<(string, string)>{
-                    ("ССК", "1"),
-                    ("Открывающие", "2")
-                },
-                new List<(string, string)>{
-                    ("Администраторы", "3"),
-                    ("Черный список", "4"),
-                }
-            };
-            return buttons;
-        }
-
-        private static List<List<(string, string)>> GetAdminStatusButtons(int status)
-        {
-            var buttons = new List<List<(string, string)>> {
-                new List<(string, string)>{("Добавить", "add") }
-            };
-            if ((Data.AmountOf(status) != 0 && status != 3) || (Data.AmountOf(status) > 1 && status == 3))
-                buttons[0].Add(("Удалить", "del"));
-            return buttons;
         }
     }
 }
---
+++ ChatBotLaundry/GetButtons.cs
@@ -0,0 +1,215 @@
+﻿using System.Collections.Generic;
+
+namespace ChatBotLaundry
+{
+    internal class GetButtons
+    {
+        //методы получения кнопок
+        internal static List<List<(string, string)>> Adm()
+        {
+            var buttons = new List<List<(string, string)>> {
+                new List<(string, string)>{
+                    ("Функции клиента", "cl"),
+                    ("Функции открывающего", "op")
+                },
+                new List<(string, string)>{
+                    ("Администрирование", "ad"),
+                    ("Отчетность", "re")
+                },
+                new List<(string, string)>{
+                    ("Выйти", "b")
+                }
+            };
+            return buttons;
+        }
+
+            internal static List<List<(string, string)>> Ad()
+            {
+                var userAdminButtons = new List<List<(string, string)>>{
+                    new List<(string, string)>
+                    {
+                        ("Пользователи", "us")
+                    },
+                    new List<(string, string)>{
+                        ("Прачка", "l")
+                    }
+                };
+                if (Data.Notes.Count != 0)
+                    userAdminButtons.Add(new List<(string, string)>
+                    {
+                        ("Записи", "n" )
+                    });
+                return userAdminButtons;
+            }
+
+                internal static List<List<(string, string)>> Us()
+                {
+                    var buttons = new List<List<(string, string)>> {
+                        new List<(string, string)>{
+                            ("ССК", "1"),
+                            ("Открывающие", "2")
+                        },
+                        new List<(string, string)>{
+                            ("Администраторы", "3"),
+                            ("Черный список", "4"),
+                        }
+                    };
+                    return buttons;
+                }
+
+                    internal static List<List<(string, string)>> Usrs(int status)
+                    {
+                        var buttons = new List<List<(string, string)>> {
+                            new List<(string, string)>{("Добавить", "add") }
+                        };
+                        if ((Data.AmountOf(status) != 0 && status != 3) || (Data.AmountOf(status) > 1 && status == 3))
+                            buttons[0].Add(("Удалить", "del"));
+                        return buttons;
+                    }
+
+                internal static List<List<(string, string)>> L()
+                {
+                    var buttons = new List<List<(string, string)>>{
+                        new List<(string, string)>
+                        {
+                            ("Изменить информацию о прачке", "infad")
+                        },
+                        new List<(string, string)>{
+                            ("Изменить время", "time")
+                        },
+                        new List<(string, string)>
+                        {
+                            ("Изменить количество машинок", "w")
+                        },
+                        new List<(string, string)>
+                        {
+                            ("Изменить пароль", "pas")
+                        }
+                    };
+                    return buttons;
+                }
+
+                    internal static List<List<(string, string)>> Infadc()
+                    {
+                        var buttons = new List<List<(string, string)>>{
+                            new List<(string, string)>{
+                                ("Отмена", "b")
+                            }
+                        };
+                        if (Data.NewInfo.Length != 0)
+                            buttons.Insert(0, new List<(string, string)>
+                            { ("Сохранить изменения" , "save")
+                            });
+                        return buttons;
+                    }
+
+                    internal static List<List<(string, string)>> Time()
+                    {
+                        var buttons = new List<List<(string, string)>> { };
+                        var i = -1;
+                        for (var t = 8; t < 23; t++)
+                        {
+                            if ((t - 8) % 5 == 0)
+                            {
+                                buttons.Add(new List<(string, string)>());
+                                i++;
+                            }
+                            buttons[i].Add((t.ToString() + ":00", t.ToString()));
+                        }
+                        return buttons;
+                    }
+
+        internal static List<List<(string, string)>> Cl(User user)
+        {
+            var clientMenuButtons = new List<List<(string, string)>> {
+                new List<(string, string)>{
+                    ("Выкл уведомления", "clnt"),
+                    ("FAQ", "info")
+                }
+            };
+
+            if (Data.Notes.FindIndex(delegate (TimeNote note)
+            {
+                return note.UserID == user.ID;
+            }
+            ) != -1)
+                clientMenuButtons.Insert(0, new List<(string, string)> { ("Отмена", "cldn") });
+            if (Data.Days.FindIndex(delegate (Day day)
+            {
+                return (day.EmptySpaces != 0) && (day.AvailableForSSK == (user.Status == 1));
+            }
+            ) != -1)
+                clientMenuButtons.Insert(0, new List<(string, string)> { ("Записаться в прачечную", "cln") });
+            if (!user.NotificationStatus)
+            {
+                var repl = clientMenuButtons[^1][0];
+                repl.Item1 = "Вкл уведомления";
+                clientMenuButtons[^1][0] = repl;
+            }
+            else
+            {
+                var repl = clientMenuButtons[^1][0];
+                repl.Item1 = "Выкл уведомления";
+                clientMenuButtons[^1][0] = repl;
+            }
+            if (user.Status != 3 && user.Status != 2)
+            {
+                clientMenuButtons.Add(new List<(string, string)> { ("Выйти", "b") });
+            }
+            return clientMenuButtons;
+        }
+
+            internal static List<List<(string, string)>> Cln(int status)
+            {
+                var buttons = new List<List<(string, string)>>();
+                for (var d = 0; d < Data.Days.Count; d++)
+                    if (Data.Days[d].IsFree && ((Data.Days[d].AvailableForSSK && status != 0) || (!Data.Days[d].AvailableForSSK && status == 0)))
+                    {
+                        var button = new List<(string, string)> { (Data.Days[d].DayOfWeekR + " " + Data.Days[d].EmptySpaces.ToString(), d.ToString()) };
+                        buttons.Add(button);
+                    }
+                return buttons;
+            }
+
+                internal static List<List<(string, string)>> Clnd(int day)
+                {
+                    var buttons = new List<List<(string, string)>>();
+                    for (var i = 0; i < Data.Days[day].HoursWashesTable.GetLength(0); i++)
+                    {
+                        if (Data.Days[day].EmptyTimes[i] != 0)
+                        {
+                            var button = new List<(string, string)> { (Data.WashesHours[i].ToString() + ":00 кол-во свободных машинок:" + Data.Days[day].EmptyTimes[i].ToString(), i.ToString()) };
+                            buttons.Add(button);
+                        }
+                    }
+                    return buttons;
+                }
+
+                    internal static List<List<(string, string)>> Clndt(int day, int time)
+                    {
+                        var buttons = new List<List<(string, string)>>();
+                        for (var i = 1; i < Data.Days[day].EmptyTimes[time] + 1; i++)
+                        {
+                            var button = new List<(string, string)> { (i.ToString() + ". ", i.ToString()) };
+                            buttons.Add(button);
+                        }
+                        return buttons;
+                    }
+
+            internal static List<List<(string, string)>> Cldn(User user)
+            {
+                var notes = Data.Notes.FindAll(delegate (TimeNote note)
+                {
+                    return note.UserID == user.ID;
+                }
+                );
+                var buttons = new List<List<(string, string)>>();
+                for (var i = 0; i < notes.Count; i++)
+                {
+                    var button = new List<(string, string)> { (notes[i].ToString() + "\n", i.ToString()) };
+                    buttons.Add(button);
+                }
+                return buttons;
+            }
+    }
+}
